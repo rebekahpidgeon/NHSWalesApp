a5886069703b8fe375fb0e1c4508f6709c0bfb5b
pTinosq <tinosps@gmail.com>
2025-03-01 21:59:13 +0000

    pscript box

--- frontend/src/components/PrescriptionBox/PrescriptionBox.tsx
+++ frontend/src/components/PrescriptionBox/PrescriptionBox.tsx
@@ -1,26 +1,26 @@
 export default function PrescriptionBox() {
   return <div className="p-4">
-    <div className="bg-slate-100 rounded-md p-2 shadow-md">
+    <div className="bg-white rounded-md p-2 shadow-md">
       <div className="flex">
         <div
           className="flex flex-col justify-center p-2"
         >
           <h2 className="font-semibold">Approved! Your order is being filled at the pharmacy</h2>
-          <p className="font-light text-sm">Custom Acne Cream</p>
+          <p className="font-light text-sm">Ibuprofen</p>
         </div>
         <div
-          className="flex flex-1/4 items-center justify-center rounded-xl"
+          className="flex items-center justify-center rounded-xl bg-slate-300 aspect-square h-full w-32 p-2"
         >
           <img
             className="w-16 aspect-square"
-            src="https://placehold.co/256x256/EEE000/31343C"
-            alt="Custom Acne Cream" />
+            src="/placeholder/ibuprofen.png"
+            alt="" />
         </div>
       </div>
-      <div>
+      <div className="pt-1">
         <div id="progress-bar-container" className="relative rounded-xl p-2">
           <div className=" absolute rounded-xl bg-nhs-dark-blue w-[62%] h-2" />
-          <div className="rounded-xl bg-slate-300 w-full h-2" />
+          <div className="rounded-xl bg-slate-200 w-full h-2" />
         </div>
         <div className="px-2 flex justify-between">
           <p className="text-sm">Requested</p>
