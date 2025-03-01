5d2b860365aa09d1d4e284f7d618509146d4b0ea
pTinosq <tinosps@gmail.com>
2025-03-01 17:06:49 +0000

    foitnend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar/Navbar"
+import PrescriptionBox from "./components/PrescriptionBox/PrescriptionBox"
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <div>
+        <PrescriptionBox />
+      </div>
     </>
   )
 }
