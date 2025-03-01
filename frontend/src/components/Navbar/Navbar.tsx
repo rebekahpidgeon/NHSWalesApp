de79adb77232b38d2d3f55afc302602564b0f339
pTinosq <tinosps@gmail.com>
2025-03-01 16:56:20 +0000

    NAVBAR YAY

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
+import Logo from '../../assets/snh-logo.png';
+
 export default function Navbar() {
   return (
-    <nav className="bg-nhs-blue text-white h-16">
-
+    <nav className="bg-nhs-blue text-white h-16 p-3 flex items-center justify-center">
+        <img src={Logo} alt="logo" className="h-full" />
     </nav>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import './styles/normalize.css'
 import './styles/index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from "react-router";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("No root element found");
+}
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
